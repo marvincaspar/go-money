c3cc4599109e9734e30e921beb140bdda4f560fa
Marvin Caspar <marvin.caspar@bertelsmann.de>
2018-08-21 16:21:27 +0200

    Add currency factory for USD and EUR

--- currency.go
+++ currency.go
@@ -9,3 +9,15 @@ type Currency struct {
 	symbol   string
 	template string
 }
+
+// https://en.wikipedia.org/wiki/ISO_4217
+
+// USD creates and returns a new Currenty instance for USD
+func USD() *Currency {
+	return &Currency{code: "USD", decimal: ".", thousand: ",", exponent: 2, symbol: "$", template: "$1"}
+}
+
+// EUR creates and returns a new Currenty instance for EUR
+func EUR() *Currency {
+	return &Currency{code: "EUR", decimal: ",", thousand: ".", exponent: 2, symbol: "€", template: "$1"}
+}
