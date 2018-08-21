e903ef0a1a1d7583d72179e6cccea5870a9c53f9
Marvin Caspar <marvin.caspar@bertelsmann.de>
2018-08-21 17:00:46 +0200

    Add money getter for amount and currency

--- money.go
+++ money.go
@@ -18,3 +18,13 @@ func New(amount int64, currency *Currency) *Money {
 		currency: currency,
 	}
 }
+
+// Currency returns the currency used by Money
+func (m *Money) Currency() *Currency {
+	return m.currency
+}
+
+// Amount returns the amount value as int64
+func (m *Money) Amount() int64 {
+	return m.amount.val
+}
