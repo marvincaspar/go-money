c7f6ac26e4b53622c75df0560d00718c02aee031
Marvin Caspar <marvin.caspar@bertelsmann.de>
2018-08-23 19:33:12 +0200

    Add money comparison methods

--- money.go
+++ money.go
@@ -1,5 +1,7 @@
 package money
 
+import "errors"
+
 // Amount stores the amounts used for calculations
 type Amount struct {
 	val int64
@@ -10,6 +12,12 @@ type Money struct {
 	amount   *Amount
 	currency *Currency
 }
+
+const (
+	GreaterThanCheckResult = 1
+	EqualCheckResult       = 0
+	LessThanCheckResult    = -1
+)
 
 // New creates and returns a new Money instance
 func New(amount int64, currency *Currency) *Money {
@@ -27,4 +35,72 @@ func (m *Money) Currency() *Currency {
 // Amount returns the amount value as int64
 func (m *Money) Amount() int64 {
 	return m.amount.val
+}
+
+// Equals checkes equality between two Money instances
+func (m *Money) Equals(money *Money) (bool, error) {
+	if err := m.assertSameCurrency(money); err != nil {
+		return false, err
+	}
+
+	return m.compare(money) == 0, nil
+}
+
+// SameCurrency checks if the given Money is equal by currency
+func (m *Money) SameCurrency(money *Money) bool {
+	return m.currency.equals(money.currency)
+}
+
+// GreaterThan checks whether the value of Money is greater than the other
+func (m *Money) GreaterThan(money *Money) (bool, error) {
+	if err := m.assertSameCurrency(money); err != nil {
+		return false, err
+	}
+
+	return m.compare(money) == GreaterThanCheckResult, nil
+}
+
+// GreaterThanOrEqual checks whether the value of Money is greater or equal than the other
+func (m *Money) GreaterThanOrEqual(money *Money) (bool, error) {
+	if err := m.assertSameCurrency(money); err != nil {
+		return false, err
+	}
+
+	return m.compare(money) >= EqualCheckResult, nil
+}
+
+// LessThan checks whether the value of Money is less than the other
+func (m *Money) LessThan(money *Money) (bool, error) {
+	if err := m.assertSameCurrency(money); err != nil {
+		return false, err
+	}
+
+	return m.compare(money) == LessThanCheckResult, nil
+}
+
+// LessThanOrEqual checks whether the value of Money is less or equal than the other
+func (m *Money) LessThanOrEqual(money *Money) (bool, error) {
+	if err := m.assertSameCurrency(money); err != nil {
+		return false, err
+	}
+
+	return m.compare(money) <= EqualCheckResult, nil
+}
+
+func (m *Money) assertSameCurrency(money *Money) error {
+	if !m.SameCurrency(money) {
+		return errors.New("Currency don't match")
+	}
+
+	return nil
+}
+
+func (m *Money) compare(money *Money) int {
+	if m.Amount() > money.Amount() {
+		return GreaterThanCheckResult
+	} else if m.Amount() < money.Amount() {
+		return LessThanCheckResult
+	}
+
+	return EqualCheckResult
 }
