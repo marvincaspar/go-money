f72cd058d190b5c6c70b14eaeed70a406ce211bb
Marvin Caspar <marvin.caspar@bertelsmann.de>
2018-10-11 18:13:27 +0200

    Add calculator

--- calculator.go
+++ calculator.go
@@ -1,14 +1,43 @@
 package money
 
-// Calculator calculates different amounts
-type Calculator struct{}
+type calculator struct{}
 
-// Add adds two amounts
-func (c *Calculator) Add(a, b *Amount) *Amount {
-	return &Amount{a.val + b.val}
+func (c *calculator) add(a, b *Amount) *Amount {
+	return &Amount{a.Value() + b.Value()}
 }
 
-// Subtract subtracts one amount from an other
-func (c *Calculator) Subtract(a, b *Amount) *Amount {
-	return &Amount{a.val - b.val}
+func (c *calculator) subtract(a, b *Amount) *Amount {
+	return &Amount{a.Value() - b.Value()}
+}
+
+func (c *calculator) multiply(a *Amount, m int64) *Amount {
+	return &Amount{a.Value() * m}
+}
+
+func (c *calculator) divide(a *Amount, d int64) *Amount {
+	return &Amount{a.Value() / d}
+}
+
+func (c *calculator) modulus(a *Amount, d int64) *Amount {
+	return &Amount{a.Value() % d}
+}
+
+func (c *calculator) allocate(a *Amount, ratio, total int) *Amount {
+	return &Amount{a.Value() * int64(ratio) / int64(total)}
+}
+
+func (c *calculator) absolute(a *Amount) *Amount {
+	if a.Value() < 0 {
+		return &Amount{-a.Value()}
+	}
+
+	return &Amount{a.Value()}
+}
+
+func (c *calculator) negative(a *Amount) *Amount {
+	if a.Value() > 0 {
+		return &Amount{-a.Value()}
+	}
+
+	return &Amount{a.Value()}
 }
